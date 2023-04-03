eaa3063f3629519835796289a5d16ca0b279a495
Eguchi <126472433+eguchoii@users.noreply.github.com>
2023-04-03 12:18:02 -0300

    Update algoritmo2.go

--- algoritmo2.go
+++ algoritmo2.go
@@ -5,15 +5,13 @@ import (
 	"fmt"
 	"os"
 	"strings"
-	
 )
 
 func main() {
 	fmt.Print("Digite uma string:")
 	scanner := bufio.NewScanner(os.Stdin)
 	if scanner.Scan() {
-		output := strings.ReplaceAll(scanner.Text()," ", "")
-		fmt.Print("String sem espaços em branco:")
+		output := strings.ReplaceAll(scanner.Text(), " ", "")
+		fmt.Println("String sem espaços em branco:", output)
 	}
-
 }
