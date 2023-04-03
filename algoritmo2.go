3a45057be41b5cde0e606285f9ff386adcd49a1f
Eguchi <126472433+eguchoii@users.noreply.github.com>
2023-04-03 10:41:54 -0300

    Update algoritmo2.go

--- algoritmo2.go
+++ algoritmo2.go
@@ -1,1 +1,19 @@
+package main
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+	
+)
+
+func main() {
+	fmt.Print("Digite uma string:")
+	scanner := bufio.NewScanner(os.Stdin)
+	if scanner.Scan() {
+		output := strings.ReplaceAll(scanner.Text()," ", "")
+		fmt.Print("String sem espaços em branco:")
+	}
+
+}
